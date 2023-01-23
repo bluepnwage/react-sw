4669c5a86eea1ae096c6944647e1992ec207ce39
bluepnwage <a.carty2555@gmail.com>
2023-01-23 11:54:16 -0400

    update service worker

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
 const addToCache = async (resources) => {
-  const cache = await caches.open("v1");
+  const cache = await caches.open("v2");
   await cache.add(resources);
 };
 
 const putInCache = async (request, response) => {
-  const cache = await caches.open("v1");
+  const cache = await caches.open("v2");
   await cache.put(request, response);
 };
 
@@ -18,6 +18,21 @@ const cacheFirst = async (request) => {
   putInCache(request, networkResponse.clone());
   return networkResponse;
 };
+
+const deleteOldCache = async (key) => {
+  await caches.delete(key);
+};
+
+const deleteCaches = async () => {
+  const cachesToKeep = ["v2"];
+  const keys = await caches.keys();
+  const filteredKeys = keys.filter((key) => !cachesToKeep.includes(key));
+  await Promise.all(filteredKeys.map(deleteOldCache));
+};
+
+self.addEventListener("active", (event) => {
+  event.waitIntil(deleteCaches);
+});
 
 self.addEventListener("install", (event) => {
   event.waitUntil(addToCache(["/index.html"]));
